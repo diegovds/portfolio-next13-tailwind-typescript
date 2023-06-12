be2a6d3bb530fb2d9d8429305888df660416883f
Diego Viana <dsantos@inf.ufsm.br>
2023-06-12 20:34:12 -0300

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import { IBM_Plex_Mono, Inter } from 'next/font/google'
+import { ReactNode } from 'react'
+import './globals.css'
+
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+})
+
+const plexMono = IBM_Plex_Mono({
+  variable: '--font-plex-mono',
+  subsets: ['latin'],
+  weight: ['400', '500'],
+})
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="pt-BR" className={`${inter.variable} ${plexMono.variable}`}>
+      <body>{children}</body>
+    </html>
+  )
+}

efd0c48f352583452ce4c6f4b8d34d53efe2ff13
Diego Viana <dsantos@inf.ufsm.br>
2023-06-30 13:03:42 -0300

    adding ProjectDetails component

---
+++ src/app/components/Pages/Project/ProjectDetails/index.tsx
@@ -0,0 +1,60 @@
+import Button from '@/app/components/Button'
+import Link from '@/app/components/Link'
+import SectionTitle from '@/app/components/SectionTitle'
+import TechBadge from '@/app/components/TechBadge'
+import { FiGlobe } from 'react-icons/fi'
+import { HiArrowNarrowLeft } from 'react-icons/hi'
+import { TbBrandGithub } from 'react-icons/tb'
+
+const ProjectDetails = () => {
+  return (
+    <section className="relative flex w-full flex-col items-center justify-end overflow-hidden px-6 py-24 pb-10 sm:min-h-[750px] sm:pb-24">
+      <div
+        className="absolute inset-0 z-[-1]"
+        style={{
+          background:
+            'url(/images/hero-bg.png) no-repeat center/cover, url(https://media.graphassets.com/7Kic5YHkQcmGrN57MSXw) no-repeat center/cover',
+        }}
+      />
+
+      <SectionTitle
+        subtitle="projetos"
+        title="BookWise"
+        className="items-center text-center sm:[&>h3]:text-4xl"
+      />
+      <p className="my-4 max-w-[640px] text-center text-sm text-gray-400 sm:my-6 sm:text-base">
+        BookWise é uma plataforma de avaliação de livros que foi desenvolvida
+        durante o bootcamp Ignite da Rocketseat. Com apenas um Figma
+        precisávamos desenvolver essa aplicação completa Full Stack com Next.js.
+      </p>
+      <div className="flex w-full max-w-[330px] flex-wrap items-center justify-center gap-2">
+        <TechBadge name="Next.js" />
+        <TechBadge name="Next.js" />
+        <TechBadge name="Next.js" />
+        <TechBadge name="Next.js" />
+        <TechBadge name="Next.js" />
+        <TechBadge name="Next.js" />
+      </div>
+      <div className="my-6 flex flex-col items-center gap-2 sm:my-12 sm:flex-row sm:gap-4">
+        <a href="http://github.com" target="_blank" rel="noopener noreferrer">
+          <Button className="min-w-[180px]">
+            <TbBrandGithub size={20} />
+            Repositório
+          </Button>
+        </a>
+        <a href="http://github.com" target="_blank" rel="noopener noreferrer">
+          <Button className="min-w-[180px]">
+            <FiGlobe size={20} />
+            Projeto online
+          </Button>
+        </a>
+      </div>
+      <Link href="/projects">
+        <HiArrowNarrowLeft size={20} />
+        Voltar para projetos
+      </Link>
+    </section>
+  )
+}
+
+export default ProjectDetails
---
+++ src/app/projects/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import ProjectDetails from '@/app/components/Pages/Project/ProjectDetails'
+
+const Project = () => {
+  return (
+    <>
+      <ProjectDetails />
+    </>
+  )
+}
+
+export default Project

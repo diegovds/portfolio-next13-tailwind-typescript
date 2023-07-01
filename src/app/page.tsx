163c76764568ad0fca5667aec6de9df457450e04
Diego Viana <dsantos@inf.ufsm.br>
2023-07-01 01:32:09 -0300

    fetching data from CMS on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,45 @@
+import { HomePageData } from '@/types/PageInfo'
+import { fetchHygraphQuery } from '@/utils/fetchHygraphQuery'
 import HeroSection from './components/Pages/Home/HeroSection'
 import HighlightedProjects from './components/Pages/Home/HighlightedProjects'
 import KnownTechs from './components/Pages/Home/KnownTechs'
 import WorkExperience from './components/Pages/Home/WorkExperience'
 
-const Home = () => {
+const getPageData = async (): Promise<HomePageData> => {
+  const query = `
+    query PageInfoQuery {
+      page(where: {slug: "home"}) {
+        introduction {
+          raw
+        }
+        technologies {
+          name
+        }
+        profilePicture {
+          url
+        }
+        socials {
+          url
+          iconSvg
+        }
+        knownTechs {
+          iconSvg
+          name
+          startDate
+        }
+      }
+    }
+  `
+
+  return fetchHygraphQuery(query, 60 * 60 * 24)
+}
+
+const Home = async () => {
+  const { page: pageData } = await getPageData()
+
   return (
     <>
-      <HeroSection />
+      <HeroSection homeInfo={pageData} />
       <KnownTechs />
       <HighlightedProjects />
       <WorkExperience />

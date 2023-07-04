dd7abd573958d3a031b8ed196bf239a2657bff43
Diego Viana <dsantos@inf.ufsm.br>
2023-07-04 16:37:18 -0300

    adding animation in ProjectCard component

--- src/app/components/Pages/Home/HighlightedProjects/ProjectCard/index.tsx
+++ src/app/components/Pages/Home/HighlightedProjects/ProjectCard/index.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import Link from '@/app/components/Link'
 import TechBadge from '@/app/components/TechBadge'
 import { Project } from '@/types/Projects'
+import { motion } from 'framer-motion'
 import Image from 'next/image'
 import { HiArrowNarrowRight } from 'react-icons/hi'
 
@@ -9,9 +12,24 @@ type ProjectCardProps = {
 }
 
 const ProjectCard = ({ project }: ProjectCardProps) => {
+  const animProps = {
+    initial: { opacity: 0, y: 50 },
+    whileInView: { opacity: 1, y: 0 },
+  }
+
   return (
-    <div className="flex flex-col gap-6 lg:flex-row lg:gap-12">
-      <div className="h-[200px] w-full sm:h-[300px] lg:min-h-full lg:w-[420px]">
+    <motion.div
+      className="flex flex-col gap-6 lg:flex-row lg:gap-12"
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <motion.div
+        className="h-[200px] w-full sm:h-[300px] lg:min-h-full lg:w-[420px]"
+        initial={{ opacity: 0, y: 100, scale: 0.5 }}
+        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+        transition={{ duration: 0.3, delay: 0.3 }}
+      >
         <Image
           width={420}
           height={304}
@@ -19,10 +37,14 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
           alt={`Thumbnail do projeto ${project.title}`}
           className="h-full w-full rounded-lg object-cover"
         />
-      </div>
+      </motion.div>
 
       <div className="flex-1 lg:py-[18px]">
-        <h3 className="flex items-center gap-3 text-lg font-medium text-gray-50">
+        <motion.h3
+          className="flex items-center gap-3 text-lg font-medium text-gray-50"
+          {...animProps}
+          transition={{ duration: 0.7 }}
+        >
           <Image
             width={20}
             height={20}
@@ -30,13 +52,25 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
             alt=""
           />
           {project.title}
-        </h3>
+        </motion.h3>
 
-        <p className="my-6 text-gray-400">{project.shortDescription}</p>
+        <motion.p
+          className="my-6 text-gray-400"
+          {...animProps}
+          transition={{ duration: 0.2, delay: 0.3 }}
+        >
+          {project.shortDescription}
+        </motion.p>
 
         <div className="mb-8 flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[350px]">
           {project.technologies.map((tech, index) => (
-            <TechBadge name={tech.name} key={index} />
+            <TechBadge
+              name={tech.name}
+              key={index}
+              initial={{ opacity: 0, scale: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.2, delay: 0.5 + index * 0.1 }}
+            />
           ))}
         </div>
 
@@ -45,7 +79,7 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
           <HiArrowNarrowRight />
         </Link>
       </div>
-    </div>
+    </motion.div>
   )
 }
 

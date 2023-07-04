7b70fb2cb82ad820b1afc4ca7568b2a8c2d437ed
Diego Viana <dsantos@inf.ufsm.br>
2023-07-04 18:03:19 -0300

    adding animation in ProjectsList component

--- src/app/components/Pages/Projects/ProjectsList/index.tsx
+++ src/app/components/Pages/Projects/ProjectsList/index.tsx
@@ -1,4 +1,8 @@
+'use client'
+
+import { fadeUpAnimation } from '@/libs/animations'
 import { Project } from '@/types/Projects'
+import { motion } from 'framer-motion'
 import Link from 'next/link'
 import ProjectCard from './ProjectCard'
 
@@ -10,9 +14,15 @@ const ProjectsList = ({ projects }: ProjectsListProps) => {
   return (
     <section className="container grid grid-cols-1 gap-x-4 gap-y-6 py-32 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))]">
       {projects.map((project, index) => (
-        <Link key={index} href={`/projects/${project.slug}`}>
-          <ProjectCard project={project} />
-        </Link>
+        <motion.div
+          key={index}
+          {...fadeUpAnimation}
+          transition={{ duration: 0.5, delay: index * 0.1 }}
+        >
+          <Link href={`/projects/${project.slug}`}>
+            <ProjectCard project={project} />
+          </Link>
+        </motion.div>
       ))}
     </section>
   )

a3dce6b6fd4d43999f41268bca9a544ee9156ef7
Diego Viana <dsantos@inf.ufsm.br>
2023-07-04 17:19:57 -0300

    Update animations.ts

--- src/libs/animations.ts
+++ src/libs/animations.ts
@@ -4,3 +4,8 @@ export const techBadgeAnimation: MotionProps = {
   initial: { opacity: 0, scale: 0 },
   whileInView: { opacity: 1, scale: 1 },
 }
+
+export const fadeUpAnimation: MotionProps = {
+  initial: { opacity: 0, y: 50 },
+  whileInView: { opacity: 1, y: 0 },
+}

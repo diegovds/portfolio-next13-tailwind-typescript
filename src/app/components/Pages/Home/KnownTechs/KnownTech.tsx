ae4f847fe5af524e1ca3a84087814ba0f2ec1b38
Diego Viana <dsantos@inf.ufsm.br>
2023-07-04 18:41:52 -0300

    fixing the animation in KnownTechs component

--- src/app/components/Pages/Home/KnownTechs/KnownTech.tsx
+++ src/app/components/Pages/Home/KnownTechs/KnownTech.tsx
@@ -1,33 +1,26 @@
-'use client'
-
 import CMSIcon from '@/app/components/CMSIcon'
 import { KnownTech as IKnownTech } from '@/types/Projects'
 import { getRelativeTimeString } from '@/utils/getRelativeTimeString'
-import { motion } from 'framer-motion'
-import { ComponentProps } from 'react'
 
-type KnownTechProps = ComponentProps<typeof motion.div> & {
+type KnownTechProps = {
   tech: IKnownTech
 }
 
-const KnownTech = ({ tech, ...props }: KnownTechProps) => {
+const KnownTech = ({ tech }: KnownTechProps) => {
   const relativeTime = getRelativeTimeString(
     new Date(tech.startDate),
     'pt-BR',
   ).replace('há', '')
 
   return (
-    <motion.div
-      className="flex flex-col gap-2 rounded-lg bg-gray-600/20 p-6 text-gray-500 transition-all hover:bg-gray-600/30 hover:text-emerald-500"
-      {...props}
-    >
+    <div className="flex flex-col gap-2 rounded-lg bg-gray-600/20 p-6 text-gray-500 transition-all hover:bg-gray-600/30 hover:text-emerald-500">
       <div className="flex items-center justify-between ">
         <p className="font-medium">{tech.name}</p>
         <CMSIcon icon={tech.iconSvg} />
       </div>
 
       <span>{relativeTime} de experiência</span>
-    </motion.div>
+    </div>
   )
 }
 
--- src/app/components/Pages/Home/KnownTechs/index.tsx
+++ src/app/components/Pages/Home/KnownTechs/index.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import SectionTitle from '@/app/components/SectionTitle'
 import { KnownTech as IKnownTech } from '@/types/Projects'
+import { motion } from 'framer-motion'
 import KnownTech from './KnownTech'
 
 type KnownTechsProps = {
@@ -10,16 +13,17 @@ const KnownTechs = ({ techs }: KnownTechsProps) => {
   return (
     <section className="container py-16">
       <SectionTitle subtitle="competências" title="Conhecimentos" />
-
-      <div className="mt-[60px] grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3">
-        {techs.map((tech, index) => (
-          <KnownTech
-            key={index}
-            tech={tech}
+      <div className="mt-[60px] grid w-full grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3">
+        {techs?.map((tech, i) => (
+          <motion.div
+            key={tech.name}
             initial={{ opacity: 0, x: -100 }}
             whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.15, delay: index * 0.1 }}
-          />
+            exit={{ opacity: 0, x: -100 }}
+            transition={{ duration: 0.15, delay: i * 0.1 }}
+          >
+            <KnownTech tech={tech} />
+          </motion.div>
         ))}
       </div>
     </section>

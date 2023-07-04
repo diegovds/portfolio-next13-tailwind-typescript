b321354fac168d3dca91baa1f3d9d7cb781b03ef
Diego Viana <dsantos@inf.ufsm.br>
2023-07-04 16:06:44 -0300

    adding animation in KnownTech component

--- src/app/components/Pages/Home/KnownTechs/KnownTech.tsx
+++ src/app/components/Pages/Home/KnownTechs/KnownTech.tsx
@@ -1,26 +1,33 @@
+'use client'
+
 import CMSIcon from '@/app/components/CMSIcon'
 import { KnownTech as IKnownTech } from '@/types/Projects'
 import { getRelativeTimeString } from '@/utils/getRelativeTimeString'
+import { motion } from 'framer-motion'
+import { ComponentProps } from 'react'
 
-type KnownTechProps = {
+type KnownTechProps = ComponentProps<typeof motion.div> & {
   tech: IKnownTech
 }
 
-const KnownTech = ({ tech }: KnownTechProps) => {
+const KnownTech = ({ tech, ...props }: KnownTechProps) => {
   const relativeTime = getRelativeTimeString(
     new Date(tech.startDate),
     'pt-BR',
   ).replace('há', '')
 
   return (
-    <div className="flex flex-col gap-2 rounded-lg bg-gray-600/20 p-6 text-gray-500 transition-all hover:bg-gray-600/30 hover:text-emerald-500">
+    <motion.div
+      className="flex flex-col gap-2 rounded-lg bg-gray-600/20 p-6 text-gray-500 transition-all hover:bg-gray-600/30 hover:text-emerald-500"
+      {...props}
+    >
       <div className="flex items-center justify-between ">
         <p className="font-medium">{tech.name}</p>
         <CMSIcon icon={tech.iconSvg} />
       </div>
 
       <span>{relativeTime} de experiência</span>
-    </div>
+    </motion.div>
   )
 }
 
--- src/app/components/Pages/Home/KnownTechs/index.tsx
+++ src/app/components/Pages/Home/KnownTechs/index.tsx
@@ -13,7 +13,13 @@ const KnownTechs = ({ techs }: KnownTechsProps) => {
 
       <div className="mt-[60px] grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3">
         {techs.map((tech, index) => (
-          <KnownTech key={index} tech={tech} />
+          <KnownTech
+            key={index}
+            tech={tech}
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.15, delay: index * 0.1 }}
+          />
         ))}
       </div>
     </section>

27b8f1669daab3232123f475c74a0d8566138c6e
Diego Viana <dsantos@inf.ufsm.br>
2023-07-05 21:46:00 -0300

    adding the projects page metadata description

--- src/app/components/Pages/Projects/PageIntroduction/index.tsx
+++ src/app/components/Pages/Projects/PageIntroduction/index.tsx
@@ -20,7 +20,7 @@ const PageIntroduction = () => {
         transition={{ duration: 0.6 }}
       >
         <p className="my-6 max-w-[640px] text-center text-sm text-gray-400 sm:text-base">
-          Aqui você poderá ver alguns dos trabalhos que eu desenvolvi. Navegue à
+          Aqui você poderá ver alguns dos projetos que eu desenvolvi. Navegue à
           vontade e explore os projetos para ver como foram criados, as
           tecnologias utilizadas e as funcionalidades implementadas.
         </p>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,13 @@
 import { ProjectsPageData } from '@/types/PageInfo'
 import { fetchHygraphQuery } from '@/utils/fetchHygraphQuery'
+import { Metadata } from 'next'
 import PageIntroduction from '../components/Pages/Projects/PageIntroduction'
 import ProjectsList from '../components/Pages/Projects/ProjectsList'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Projetos',
+  description:
+    'Aqui você poderá ver alguns dos projetos que eu desenvolvi. Navegue à vontade e explore os projetos para ver como foram criados, as tecnologias utilizadas e as funcionalidades implementadas.',
 }
 
 const getPageData = async (): Promise<ProjectsPageData> => {

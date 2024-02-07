add108a8d78e20efe2cfb49a426486dd749b2c33
Diego Viana <dsantos@inf.ufsm.br>
2024-02-07 18:06:34 -0300

    Update fetchHygraphQuery.ts

--- src/utils/fetchHygraphQuery.ts
+++ src/utils/fetchHygraphQuery.ts
@@ -1,10 +1,16 @@
-export const fetchHygraphQuery = async <T>(query: string): Promise<T> => {
+export const fetchHygraphQuery = async <T>(
+  query: string,
+  revalidate?: number,
+): Promise<T> => {
   const response = await fetch(process.env.HYGRAPH_URL!, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
       Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`,
+    },
+    next: {
+      revalidate,
     },
     body: JSON.stringify({
       query,

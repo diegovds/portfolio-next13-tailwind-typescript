3cd46b3d2c394dfd636cacbdf848a68e00b72363
Diego Viana <dsantos@inf.ufsm.br>
2025-08-28 15:17:07 -0300

    Update index.tsx

--- src/app/components/Pages/Projects/ProjectsList/index.tsx
+++ src/app/components/Pages/Projects/ProjectsList/index.tsx
@@ -11,9 +11,11 @@ type ProjectsListProps = {
 }
 
 const ProjectsList = ({ projects }: ProjectsListProps) => {
+  const orderedProjects = [...projects].sort((a, b) => a.order - b.order)
+
   return (
     <section className="container grid grid-cols-1 gap-x-4 gap-y-6 py-32 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))]">
-      {projects.map((project, index) => (
+      {orderedProjects.map((project, index) => (
         <motion.div
           key={index}
           {...fadeUpAnimation}
